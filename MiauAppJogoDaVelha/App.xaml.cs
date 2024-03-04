7d028857fd2c67b3fd6acf64621f72f861df1abf
Usuario <Usuario@CPD03-M01>
2024-03-04 15:10:38 -0300

    Adicionar arquivos de projeto.

---
+++ MiauAppJogoDaVelha/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MiauAppJogoDaVelha
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
